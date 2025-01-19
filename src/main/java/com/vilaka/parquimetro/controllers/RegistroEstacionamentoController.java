42e98ae2efeb0e8920672837ed13f9875ff00d79
pativilaka <p.vilaka@gmail.com>
2025-01-19 18:43:44 -0300

    Implementa Post entrada no RegistroEstacionamentoController

--- src/main/java/com/vilaka/parquimetro/controllers/RegistroEstacionamentoController.java
+++ src/main/java/com/vilaka/parquimetro/controllers/RegistroEstacionamentoController.java
@@ -1,17 +1,46 @@
 package com.vilaka.parquimetro.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vilaka.parquimetro.dtos.CobrancaDTO;
+import com.vilaka.parquimetro.dtos.RegistroEstacionamentoDTO;
+import com.vilaka.parquimetro.entidades.Cobranca;
+import com.vilaka.parquimetro.entidades.RegistroEstacionamento;
+import com.vilaka.parquimetro.services.RegistroEstacionamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping(value = "/estacionamento")
 public class RegistroEstacionamentoController {
 
+    @Autowired
+    private RegistroEstacionamentoService registroEstacionamentoService;
 
-    @GetMapping
-    public String teste(){
-        return "Oi, parque";
+    @PostMapping("/entrada")
+    public ResponseEntity<RegistroEstacionamentoDTO> registrarEntrada(@RequestBody RegistroEstacionamentoDTO registroDTO) {
+        RegistroEstacionamento registro = registroEstacionamentoService.registrarEntrada(registroDTO);
+        RegistroEstacionamentoDTO responseDTO = new RegistroEstacionamentoDTO(
+                registro.getId(),
+                registro.getEntrada(),
+                null, // A saída ainda não foi registrada
+                registro.getVeiculo().getId(),
+                registro.getFilial().getId()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @PostMapping("/{registroId}/saida")
+    public ResponseEntity<CobrancaDTO> registrarSaida(@PathVariable Long registroId, @RequestBody LocalDateTime saida) {
+        Cobranca cobranca = registroEstacionamentoService.registrarSaida(registroId, saida);
+        CobrancaDTO cobrancaDTO = new CobrancaDTO(
+                cobranca.getId(),
+                cobranca.getValor(),
+                cobranca.isPago(),
+                cobranca.getRegistro().getId()
+        );
+        return ResponseEntity.ok(cobrancaDTO);
     }
 }
